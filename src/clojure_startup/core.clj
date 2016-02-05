4790bc0b11a24cd8567c6793a7d0064ec74fdf9a
kenvuo <kenvuo@knowit.no>
2016-02-05 21:42:08 +0100

    Done with chapter 3

--- src/clojure_startup/core.clj
+++ src/clojure_startup/core.clj
@@ -237,3 +237,82 @@
 (def inc3 (inc-maker 3))
 
 (inc3 7)
+
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part
+  "matching"
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of  maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+(symmetrize-body-parts asym-hobbit-body-parts)
+
+
+(loop [iteration 0]
+  (println (str "Iteration " iteration))
+  (if (> iteration 3)
+    (println "Goodbye!")
+    (recur (inc iteration))))
+
+(re-find #"^left-" "left-eye")
+
+
+(re-find #"^left-" "cleft-chin")
+
+
+(re-find #"^left-" "wongleblart")
+
+(matching-part {:name "left-eye" :size 1})
+
+
+(reduce + [1 2 3 4])
+
+(reduce + 15 [1 2 3 4])
+
+(defn dec-maker
+  "decremental maker"
+  [decx]
+  (fn [x] (- x decx)))
+
+(def dec9 (dec-maker 9))
+(dec9 10)
+
+(defn mapset
+  [function args]
+  (let [x (set args)]
+    (map function x)))
+
+(mapset inc [1 1 2 2]) 
+; => #{2 3} 
+  
